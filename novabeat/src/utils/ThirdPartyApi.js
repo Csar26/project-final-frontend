1214e73b4a7f2951a919576c994cea3e6d0fdb62
Cesar 26 <cesarauge26@gmail.com>
2024-11-26 18:51:07 -0500

    improved about component & token api added

--- novabeat/src/utils/ThirdPartyApi.js
+++ novabeat/src/utils/ThirdPartyApi.js
@@ -1,7 +1,9 @@
+
+{/*spotyfi id = a2a7bc11bf074bdf94535cb0bf930795*/}
 class ThirdPartyApi {
   constructor(url) {
     this._url = url;
-    this._token = "ask for token";
+    this._token = "a2a7bc11bf074bdf94535cb0bf930795";
   }
 
   setToken(token) {
@@ -14,7 +16,7 @@ class ThirdPartyApi {
     try {
       const res = await fetch(`${this._url}home`, {
         headers: {
-          authorization: this._token,
+          authorization: this._token || `Bearer ${this._token}`,
         },
       });
 
