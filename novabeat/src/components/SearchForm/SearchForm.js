de5fa29c978e829ed5c9284f8d94606626716eb5
christian garcia <christian.garcia@labbe.io>
2024-12-03 12:03:11 -0600

    ajustes para usar api de spotify

--- novabeat/src/components/SearchForm/SearchForm.js
+++ novabeat/src/components/SearchForm/SearchForm.js
@@ -1,40 +1,34 @@
 import React, { useState, useEffect } from "react";
-import ThirdPartyApi from "../../utils/ThirdPartyApi"
-
-
+import thirdpartyapi from "../../utils/ThirdPartyApi";
 
 export default function Searchform() {
-  const [searchArtist, setSearchArtist] = useState('');
+  const [searchArtist, setSearchArtist] = useState("");
 
   const handleChange = (event) => {
-      setSearchArtist(event.target.value);
+    setSearchArtist(event.target.value);
   };
 
   const handleSubmit = (event) => {
-       event.preventDefault();
-       ThirdPartyApi(searchArtist).then((data) => {
-            console.log("APIA response", data)
-       })
-       
-       
-      
+    event.preventDefault();
+    console.log('handleSubmit', searchArtist);
+    thirdpartyapi.search(searchArtist).then((data) => {
+      console.log("APIA response", data);
+    });
   };
 
-
   return (
-      <div>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="Search for Artist"></label>
-      <input
-        placeholder="Search your favorite artist"
-        name="name"
-        type="text"
-        value={searchArtist}
-        onChange={handleChange}
-        className="search-form"
-      />
-    </form>
-    {searchArtist && <ThirdPartyApi searchArtist={searchArtist}/>}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Search for Artist"></label>
+        <input
+          placeholder="Search your favorite artist"
+          name="name"
+          type="text"
+          value={searchArtist}
+          onChange={handleChange}
+          className="search-form"
+        />
+      </form>
     </div>
   );
 }
